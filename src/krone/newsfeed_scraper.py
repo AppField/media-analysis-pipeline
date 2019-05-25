e236abf337fa0df7b322a2286a10863f5354ff0d
= <roman@muehlfeldner.net>
2019-05-25 12:14:16 +0200

    Created working Krone workflow template

--- src/krone/newsfeed_scraper.py
+++ src/krone/newsfeed_scraper.py
@@ -2,30 +2,22 @@ from bs4 import BeautifulSoup
 import urllib.request
 import re
 
-class Article_Scraper():
-    url = 'https://www.krone.at/'
-
-    def get_soup(self, url):
+def get_soup(url):
         page = urllib.request.urlopen(url)
         return BeautifulSoup(page, 'html.parser')
 
-    def get_articles(self):
-        try:
-            soup = self.get_soup(url)    
+def query_newsfeed():
+    url = 'https://www.krone.at/'
+    soup = get_soup(url)
+
+    items = soup.find_all('div', {'class': 'newsticker__item'})
             
-            items = soup.find_all('div', {'class': 'newsticker__item'})
-            
-            links = [ item.find('a')['href'] for item in items ]    
-                    
-            for link in links:
-                yield self.get_soup(link)           
-            
-        except:
-            print("An error occured.")
+    links = [ item.find('a')['href'] for item in items ]    
+
+    print('\n'.join(links)) 
 
 def main():
-    Article_Scraper()
-
+    query_newsfeed()
 
 if __name__ == "__main__":
     main()
