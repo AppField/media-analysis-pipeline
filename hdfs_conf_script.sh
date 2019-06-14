69508ad4d1bb0d7d11fe78173560d313c4f9d47d
jojopirker <j.pirker@gmail.com>
2019-06-14 10:35:06 +0200

    Update hdfs_conf_script.sh

--- hdfs_conf_script.sh
+++ hdfs_conf_script.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 docker cp namenode:/etc/hadoop/core-site.xml hdfs_conf/core-site.xml
 docker cp namenode:/etc/hadoop/hdfs-site.xml hdfs_conf/hdfs-site.xml
 docker cp hdfs_conf/hdfs-site.xml nifi:/hdfs-site.xml
